0df3786cceec081b3efe99b578024921e729e08c
Wolfgang Wenzel <wolfgang.wenzel@kit.edu>
2026-02-26 09:19:31 +0100

    Comment out unused logging statements in `runner_utils` to clean up logs.

--- src/simstack/util/runner_utils.py
+++ src/simstack/util/runner_utils.py
@@ -132,8 +132,8 @@ async def clean_slurm_info(resource: Resource, user: str = None):
             # else:
             running_jobs = await context.db.engine.find(SlurmInfo, SlurmInfo.resource.value == resource.value)
             # logger.info(f"Found {running_jobs} slurm info entries for {resource}")
-            logger.info(f"Active job IDs: {active_job_ids} Slurm info IDs: {[job.job_id for job in running_jobs]}")
-            logger.info(f"User: {user} resource: {resource} ")
+            # logger.info(f"Active job IDs: {active_job_ids} Slurm info IDs: {[job.job_id for job in running_jobs]}")
+            # logger.info(f"User: {user} resource: {resource} ")
 
             # Delete entries for jobs that are no longer running
             for job in running_jobs:
