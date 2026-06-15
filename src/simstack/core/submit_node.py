f1d870bfa8167bf77c1a684e8e0e2a30d96100e4
Wolfgang Wenzel <wenzel.int@gmail.com>
2026-06-15 23:07:28 +0200

    Update `cp` command to preserve attributes and target recovery directory
    Modified `submit_node.py` to replace `cp -r` with `cp -a`, ensuring file attributes are preserved. Updated target directory path to `work_dir/recovery` for improved organization.

--- src/simstack/core/submit_node.py
+++ src/simstack/core/submit_node.py
@@ -81,7 +81,7 @@ async def submit_node(registry_entry: NodeRegistry) -> None:
                 await context.db.save(registry_entry)
                 return
 
-            full_sync_data = sync_data_start + f'cp -r {tmp_dir} {work_dir}\n' + sync_data_end
+            full_sync_data = sync_data_start + f'cp -a {tmp_dir}/. {work_dir}/recovery\n' + sync_data_end
 
             logger.info(f"task_id: {task_id} full_sync_data {full_sync_data}")
             slurm_parameters.startup_commands.append(full_sync_data)
