0bb39075f3e1302ca3c2e51944f391bdb1847160
Wolfgang Wenzel <wenzel.int@gmail.com>
2026-06-26 19:57:22 +0200

    Add debug print for project_root and adjust path handling in context.py

--- src/simstack/core/context.py
+++ src/simstack/core/context.py
@@ -139,9 +139,12 @@ class GlobalState:
         if project_root is None:  # maybe None was passed
             project_root = find_project_root()
 
+
+        project_root = Path(project_root)
         kwargs["project_root"] = project_root
 
-        simstack_toml_path = Path(project_root) / "simstack.toml"
+        print("Project root: ", project_root, "")
+        simstack_toml_path = project_root / "simstack.toml"
         toml_reader = TomlReader(project_root, config_file=Path(simstack_toml_path).resolve())
 
         db_name: str | None = kwargs.get("db_name", None)
