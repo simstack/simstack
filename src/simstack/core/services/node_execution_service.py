fb19703db9f06c4249c991ab87dad0e9bd9bb73d
Wolfgang Wenzel <wenzel.int@gmail.com>
2026-07-26 11:27:48 +0200

    Update parameter handling and logging for queue integration
    Added `queue` parameter to `Parameters` in docker example and integrated its handling in the node execution service. Improved logging to reflect queue usage and docker execution status.

--- src/simstack/core/services/node_execution_service.py
+++ src/simstack/core/services/node_execution_service.py
@@ -97,6 +97,9 @@ class NodeExecutionService(BaseService):
                 )
                 return False
 
+            logger.info(
+                f"Running node task_id: {registry_entry.id} on resource {context.config.resource} with {queue} queue and docker: {registry_entry.parameters.in_docker} "
+            )
             if registry_entry.parameters.in_docker:
                 return await run_docker(registry_entry)
 
